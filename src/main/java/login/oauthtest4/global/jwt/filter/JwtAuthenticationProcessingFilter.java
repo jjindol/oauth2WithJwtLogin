5fa75db84c30273b984b1ea64758e1a4ead170fa
jjindol <andy15948@naver.com>
2024-06-29 17:00:30 +0900

    feat: 주석 추가

--- src/main/java/login/oauthtest4/global/jwt/filter/JwtAuthenticationProcessingFilter.java
+++ src/main/java/login/oauthtest4/global/jwt/filter/JwtAuthenticationProcessingFilter.java
@@ -26,6 +26,10 @@ import java.io.IOException;
  *
  * 기본적으로 사용자는 요청 헤더에 AccessToken만 담아서 요청
  * AccessToken 만료 시에만 AcessToken과 함께 RefreshToken을 요청 헤더에 함께 요청
+ *
+ * 1. RT(X), AT(O) -> 인증 성공 처리, RT을 재발급하지 않는다.
+ * 2. RT(X), AT(X) -> 인증 실패 처리, 403 ERROR
+ * 3. RT(O) -> DB의 RT와 비교해 일치하면 AT & RT 재발급(RTR 방식), 인증 성공 처리는 하지 않고 실패 처리
  */
 
 
@@ -45,24 +49,47 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         if (request.getRequestURI().equals(NO_CHECK_URL)) {
-            filterChain.doFilter(request, response);
-            return;
+            filterChain.doFilter(request, response); // "/login" 요청이 드어오면, 다음 필터 호출
+            return; //return 으로 이후 현재 필터 진행 막기 (안해주면 아래로 내려가서 계속 필터 진행시킴)
         }
 
+        /**
+         * 사용자 요청 헤더에서 RefreshToken 추출
+         * RefreshToken이 없거나 유효하지 않다면(DB에 저장된 RefreshToken과 다르면) null 반환
+         * 사용자의 요청 헤더에 RefreshToken이 있는 경우는, AccessToken이 만료되어 요청한 경우밖에 없다!!
+         * 따라서 위의 경우를 제외하면 추출한 RefreshToken은 모두 null
+         */
         String refreshToken = jwtService.extractRefreshToken(request)
                 .filter(jwtService::isTokenValid)
                 .orElse(null);
 
+        /**
+         * RefreshToken이 요청 헤더에 존재했다면, 사용자가 AccessToken이 만료되어 RefreshToken까지 보낸 것
+         * DB의 RefreshToken과 일치하는지 판단 후, 일치하면 AccessToken 재발급
+         */
         if (refreshToken != null) {
             checkRefreshTokenAndReIssueAccessToken(response, refreshToken);
-            return;
+            return; //RT을 보낸 경우에는 AT을 재발급 하고, 인증 처리는 하지 않기 위해 바로 return 으로 필터 진행을 막는다
         }
 
+
+        /**
+         * RefreshToken이 없거나 유효하지 않다면, AccessToken을 검사하고 인증을 처리하는 로직 수행
+         * AccessToken이 없거나 유효하지 않다면, 인증 객체가 담기지 않은 상태로 다음 필터로 넘어가기 때문에 403 에러
+         * AccessToken이 유효하면, 인증 객체가 담긴 상태로 넘어가기 때문에 인증 성공
+         */
         if (refreshToken == null) {
             checkAccessTokenAndAuthentication(request, response, filterChain);
         }
     }
 
+    /**
+     * [리프레시 토큰으로 유저 정보 찾기 & 액세스/리프레시 토큰 재발급 메서드]
+     * 파라미터로 들어온 헤더에서 추출한 리프레시 토큰으로 DB에서 유저를 찾고, 해당 유저가 있다면
+     * JwtService.createAccessToken()으로 AccessToken 생성
+     * reIssueRefreshToken()로 리프레시 토큰 재발급 & DB에 리프레시 토큰 업데이트 메서드 호출
+     * 그 후 JwtService.sendAccessTokenAndRefreshToken() 으로 응답 헤더에 보내기
+     */
     public void checkRefreshTokenAndReIssueAccessToken(HttpServletResponse response, String refreshToken) {
         userRepository.findByRefreshToken(refreshToken)
                 .ifPresent(user -> {
@@ -73,6 +100,11 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
                 });
     }
 
+    /**
+     * [리프레시 토큰 재발급 & DB에 리프레시 토큰 업데이트 메서드]
+     * jwtService.createRefreshToken()으로 리프레시 토큰 재발급 후
+     * DB에 재발급 한 리프레시 토큰 업데이트 후 Flush
+     */
     private String reIssuedRefreshToken(User user) {
         String reIssuedRefreshToken = jwtService.createRefreshToken();
         user.updateRefreshToken(reIssuedRefreshToken);
@@ -80,6 +112,14 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
         return reIssuedRefreshToken;
     }
 
+    /**
+     * [액세스 토큰 체크 & 인증 처리 메서드]
+     * request에서 extractAccessToken()으로 액세스 토큰 추출 후, isTokenValid()로 유효한 토큰인지 검증
+     * 유효 -> 액세스 토큰에서 Email을 추출한 후 findByEmail()로 해당 이메일을 사용하는 유저 객체 반환
+     * 그 유저 객체를 saveAuthentication()으로 인증 처리하여
+     * 인증 허가된 객체를 SecurityContextHolder에 담기
+     * 그 후 다음 인증 필터로 진행
+     */
     public void checkAccessTokenAndAuthentication(HttpServletRequest request, HttpServletResponse response,
                                                   FilterChain filterChain) throws ServletException, IOException {
         log.info("checkAccessTokenAndAuthentication() 호출");
@@ -92,9 +132,25 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
+    /**
+     * [인증 허가 메서드]
+     * 파라미터의 유저: 우리가 만든 회윈 객체 / 빌더의 유저: UserDetails의 User 객체
+     *
+     * new UsernamePasswordAuthenticationToken()로 인증 객체인 Authentication 객체 생성
+     * UsernamePasswordAuthenticationToken의 파라미터
+     * 1. 위에서 만든 UserDetailsUser 객체 (유저 정보)
+     * 2. credential(보통 비밀번호로, 인증 시에는 보통 null로 제거)
+     * 3. Collection < ? extends GrantedAuthority>로,
+     * UserDetails의 User 객체 안에 Set<GrantedAuthority> authorities이 있어서 getter로 호출한 후,
+     * new NullAuthoritiesMapper()로 GrantedAuthoritiesMapper 객체를 생성하고 mapAuthorities()에 담기
+     *
+     * SecurityContextHolder.getContext()로 SecurityContext를 꺼낸 후,
+     * setAuthentication()을 이용해 위에서 만든 Authentication 객체에 대한 인증 허가 처리
+     */
+
     public void saveAuthentication(User myUser) {
         String password = myUser.getPassword();
-        if (password == null) {
+        if (password == null) { //소셜 로그인 유저의 PW를 임의로 설정해 소셜 로그인 유저도 인증 되도록 설정
             password = PasswordUtil.generateRandomPassword();
         }
 
@@ -111,17 +167,3 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
         SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
