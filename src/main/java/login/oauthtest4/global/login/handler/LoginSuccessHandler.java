081e248119b898d86b77f979bfbb5d9cd895da2f
jjindol <andy15948@naver.com>
2024-07-02 15:26:57 +0900

    JSON 로그인 성공 시 핸들러

---
+++ src/main/java/login/oauthtest4/global/login/handler/LoginSuccessHandler.java
@@ -0,0 +1,48 @@
+package login.oauthtest4.global.login.handler;
+
+import login.oauthtest4.domain.user.repository.UserRepository;
+import login.oauthtest4.global.jwt.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@RequiredArgsConstructor
+public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    @Value("${jwt.access.expiration}")
+    private String accessTokenExpiration;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) {
+        String email = extractUsername(authentication); //인증 정보에서 Username(email) 추출
+        String accessToken = jwtService.createAccessToken(email);
+        String refreshToken = jwtService.createRefreshToken();
+
+        jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken);
+
+        userRepository.findByEmail(email)
+                .ifPresent(user -> {
+                    user.updateRefreshToken(refreshToken);
+                    userRepository.saveAndFlush(user);
+                });
+        log.info("로그인에 성공하였습니다. 이메일: {}", email);
+        log.info("로그인에 성공하였습니다. AccessToken: {}", accessToken);
+        log.info("발급된 AccessToken 만료 기간: {}", accessTokenExpiration);
+    }
+
+    private String extractUsername(Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        return userDetails.getUsername();
+    }
+}
